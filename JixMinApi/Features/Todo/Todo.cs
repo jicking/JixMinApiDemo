4fdb89bba1af42d001aa6aac98f64b3a39782930
jicking <7297029+jicking@users.noreply.github.com>
2024-04-08 10:30:44 +0800

    add endpoint

---
+++ JixMinApi/Features/Todo/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace JixMinApi.Features.Todo;
+
+public class Todo
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public bool IsComplete { get; set; }
+
+}
