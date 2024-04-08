c31b2be9c6e988de4247755b3f8428409f8ff0f1
jicking <7297029+jicking@users.noreply.github.com>
2024-04-08 11:30:38 +0800

    Add failing tests

---
+++ JixMinApiTests/Features/Todo/TodoApiTests.cs
@@ -0,0 +1,19 @@
+﻿using JixMinApi.Features.Todo;
+using Xunit;
+
+namespace JixMinApi.Features.Todo.Tests;
+
+public class TodoApiTests
+{
+    [Fact()]
+    public void GetAllTodosTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+
+    [Fact()]
+    public void CreateTodoTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+}
