61632daecc9b661ff4c23724f4ceffa5a7f5d28d
jicking <7297029+jicking@users.noreply.github.com>
2024-04-18 10:41:43 +0800

    use IReadOnlyList for returned collection's type

--- JixMinApi/Features/Todo/DTOs.cs
+++ JixMinApi/Features/Todo/DTOs.cs
@@ -3,6 +3,6 @@
 public record TodoDto(Guid Id, string Name, bool IsComplete);
 public record TodoCreateDto(string Name, bool IsComplete);
 
-public record ValidationErrorDto(ValidationErrorItem[] ValidationErrors);
+public record ValidationErrorDto(IReadOnlyList<ValidationErrorItem> ValidationErrors);
 public record ValidationErrorItem(string Name, string Message);
 
--- JixMinApi/Features/Todo/Queries/GetAllTodosQuery.cs
+++ JixMinApi/Features/Todo/Queries/GetAllTodosQuery.cs
@@ -3,16 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace JixMinApi.Features.Todo.Queries;
 
-public record GetAllTodosQuery : IRequest<List<TodoDto>>;
+public record GetAllTodosQuery : IRequest<IReadOnlyList<TodoDto>>;
 
-public class GetAllTodosQueryHandler : IRequestHandler<GetAllTodosQuery, List<TodoDto>>
+public class GetAllTodosQueryHandler : IRequestHandler<GetAllTodosQuery, IReadOnlyList<TodoDto>>
 {
     private readonly TodoDb _db;
     private readonly ILogger<GetAllTodosQueryHandler> _logger;
 
     public GetAllTodosQueryHandler(TodoDb db, ILogger<GetAllTodosQueryHandler> logger) => (_db, _logger) = (db, logger);
 
-    public async Task<List<TodoDto>> Handle(GetAllTodosQuery request, CancellationToken cancellationToken)
+    public async Task<IReadOnlyList<TodoDto>> Handle(GetAllTodosQuery request, CancellationToken cancellationToken)
     {
         _logger.LogDebug($"Start query");
         var data = await _db.Todos
